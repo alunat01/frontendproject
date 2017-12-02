bbe9e948799efb9f5fc96d4f3422017995e47fd8
alunat01 <p14151423@my365.dmu.ac.uk>
2017-12-02 20:19:11 +0000

    Sea Turtle Image responsiveness trial on phone

--- myscripts.js
+++ myscripts.js
@@ -18,5 +18,30 @@ function initMap() {
   });
     
 }
-   
+
+// get the mPopup
+let mpopup = document.getElementById('mpopupBox');
+
+// get the link that opens the mPopup
+let mpLink = document.getElementById("mpopupLink");
+
+// get the close action element
+let close = document.getElementsByClassName("close")[0];
+
+// open the mPopup once the link is clicked
+mpLink.onclick = function() {
+    mpopup.style.display = "block";
+}
+
+// close the mPopup once close element is clicked
+close.onclick = function() {
+    mpopup.style.display = "none";
+}
+
+// close the mPopup when user clicks outside of the box
+window.onclick = function(event) {
+    if (event.target == mpopup) {
+        mpopup.style.display = "none";
+    }
+}
 
