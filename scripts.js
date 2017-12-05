dde6f658539e0033d658598e2b60810e13819bdd
alunat01 <p14151423@my365.dmu.ac.uk>
2017-12-05 21:04:56 +0000

    Tried writing JS but stuck so will ask for help

--- scripts.js
+++ scripts.js
@@ -1,3 +1,61 @@
+/* My own Modal image
+    
+    let modal = document.getElementById('mymodal');
+    let img = document.getElementById('myimg');
+    let modalImg = document.getElementById('img01');
+    let captionText = document.getElementById('caption');
+    
+    
+    img.addEventListener("click", modal);
+    
+    let image = false
+    
+function modal () {
+  if (image) {
+    img01...................................
+  }
+   else {
+     img01........................................
+     
+     toggle=true
+
+
+
+
+}
+}
+*/
+
+
+
+/* From W3school - Modal gallery
+
+
+// Get the modal
+    var modal = document.getElementById('mymodal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById('myimg');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+
+*/
+
+
 (function () {
     
     "use strict";
