32ac5270501095d8f85a7c4259f2fa0f2dfc1c68
alunat01 <p14151423@my365.dmu.ac.uk>
2017-12-28 02:03:30 +0000

    Odd final changes and indentations of codes

--- scripts.js
+++ scripts.js
@@ -13,22 +13,21 @@
 
 (function () {
 
-    let menuButton = document.getElementById("menu");
-    let navMenu = document.getElementById("nav-menu");
+	let menuButton = document.getElementById("menu");
+	let navMenu = document.getElementById("nav-menu");
 
-  menuButton.addEventListener("click", toggleMenu);
+	menuButton.addEventListener("click", toggleMenu);
 
-  let toggle = false; // hidden at first
-  function toggleMenu() {
-    if (toggle) { // true: it's visible
-      navMenu.classList.remove("show-menu"); // hide it
-      toggle = false
-    }
-    else { // false: it's hidden
-      navMenu.classList.add("show-menu"); // show it
-      toggle = true
-    }
-  }
+	let toggle = false; // hidden at first
+	function toggleMenu() {
+		if (toggle) { // true: it's visible
+			navMenu.classList.remove("show-menu"); // hide it
+			toggle = false
+		} else { // false: it's hidden
+			navMenu.classList.add("show-menu"); // show it
+			toggle = true
+		}
+	}
 
 })();
 
@@ -46,24 +45,24 @@
 
 
 function initMap() {
-  let costarica = {
-    lat: 10.540428,
-    lng: -83.498196
-  };
-  let mapDemo = document.getElementById("map");
+	let costarica = {
+		lat: 10.540428,
+		lng: -83.498196
+	};
+	let mapDemo = document.getElementById("map");
 
-  // this sets the default location for when the map is first loaded
-  let map = new google.maps.Map(mapDemo, {
-    zoom: 9,
-    center: costarica
-  });
+	// this sets the default location for when the map is first loaded
+	let map = new google.maps.Map(mapDemo, {
+		zoom: 9,
+		center: costarica
+	});
 
-  // these set different markers you want to show on your map
-  let markerCR = new google.maps.Marker({
-    position: costarica,
-    map: map,
-    title: "Release Site"
-  });
+	// these set different markers you want to show on your map
+	let markerCR = new google.maps.Marker({
+		position: costarica,
+		map: map,
+		title: "Release Site"
+	});
 
 }
 
@@ -80,22 +79,18 @@ function initMap() {
 ......................................................*/
 
 
-
-
-
-
 (function () {
- 
-  let myName = document.getElementById("my-name");
-  let userName = document.getElementById("user-name"); 
-  let getName = document.getElementById("get-name"); 
 
-  function PerformGreeting(ev) {
-    myName.innerHTML = userName.value;
-    ev.preventDefault();
-  }
- 
-  getName.addEventListener("submit", PerformGreeting);
+	let myName = document.getElementById("my-name");
+	let userName = document.getElementById("user-name");
+	let getName = document.getElementById("get-name");
+
+	function PerformGreeting(ev) {
+		myName.innerHTML = userName.value;
+		ev.preventDefault();
+	}
+
+	getName.addEventListener("submit", PerformGreeting);
 
 
 }());
@@ -113,11 +108,10 @@ Pop-up window for Adoption Form - Help from Dave in Lab
 ......................................................*/
 
 
-
 let adoptAlert = document.getElementById("adopt-submit");
 
 adoptAlert.addEventListener("click", adopt);
 
 function adopt() {
-    alert('CONGRATLATIONS! You have officially adopted a Turtle. We will be sending a package in the post with details of the turtle you have adopted and a free turtle toy!');
+	alert('CONGRATLATIONS! You have officially adopted a Turtle. We will be sending a package in the post with details of the turtle you have adopted and a free turtle toy!');
 }
